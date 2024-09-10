4dd5ef84e6c870082fc98b727f75cef7fc77d30c
Jona Avalos <jonavalos17@gmail.com>
2024-09-10 00:00:28 -0600

    compila (comentado "error")

--- src/main/java/pos/presentation/historico/View.java
+++ src/main/java/pos/presentation/historico/View.java
@@ -40,8 +40,8 @@ public class View implements PropertyChangeListener {
         listFacturas.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                int row = listFacturas.getSelectedRow();
-                controller.editFacturas(row);
+                //int row = listFacturas.getSelectedRow();
+                //controller.editFacturas(row);
                 //categorias.setSelectedIndex(row);
             }
         });
@@ -49,7 +49,7 @@ public class View implements PropertyChangeListener {
         listLineas.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                int row = listLineas.getSelectedRow();
+                //int row = listLineas.getSelectedRow();
                 //controller.edit(row);
             }
         });
@@ -73,25 +73,25 @@ public class View implements PropertyChangeListener {
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
         switch (evt.getPropertyName()) {
-            case Model.LISTLINEASLISTADO:
-                int[] colsf = {TableModelFacturas.NUMERO, TableModelFacturas.CLIENTE, TableModelFacturas.CAJERO, TableModelFacturas.FECHA, TableModelFacturas.IMPORTE};
-                listFacturas.setModel(new TableModelFacturas(colsf, model.getListLineasListado()));
-                listFacturas.setRowHeight(30);
-                TableColumnModel columnModelFacturas = listFacturas.getColumnModel();
-                columnModelFacturas.getColumn(0).setPreferredWidth(150);
-                columnModelFacturas.getColumn(1).setPreferredWidth(150);
-                break;
-            case Model.LISTLINEASNORMALES:
-                int[] colsl = {TableModelLineas.CODIGO, TableModelLineas.ARTICULO, TableModelLineas.CATEGORIA, TableModelLineas.CANTIDAD, TableModelLineas.PRECIO, TableModelLineas.DESCUENTO, TableModelLineas.NETO, TableModelLineas.IMPORTE};
-                listLineas.setModel(new TableModelLineas(colsl, model.getListLineasNormales()));
-                listLineas.setRowHeight(30);
-                TableColumnModel columnModelLineas = listLineas.getColumnModel();
-                columnModelLineas.getColumn(0).setPreferredWidth(150);
-                columnModelLineas.getColumn(1).setPreferredWidth(150);
-                break;
-            case Model.FILTER:
-               Cliente.setText(model.getFilter().getNombre());
-                break;
+//            case Model.LISTLINEASLISTADO:
+//                int[] colsf = {TableModelFacturas.NUMERO, TableModelFacturas.CLIENTE, TableModelFacturas.CAJERO, TableModelFacturas.FECHA, TableModelFacturas.IMPORTE};
+//                listFacturas.setModel(new TableModelFacturas(colsf, model.getListLineasListado()));
+//                listFacturas.setRowHeight(30);
+//                TableColumnModel columnModelFacturas = listFacturas.getColumnModel();
+//                columnModelFacturas.getColumn(0).setPreferredWidth(150);
+//                columnModelFacturas.getColumn(1).setPreferredWidth(150);
+//                break;
+//            case Model.LISTLINEASNORMALES:
+//                int[] colsl = {TableModelLineas.CODIGO, TableModelLineas.ARTICULO, TableModelLineas.CATEGORIA, TableModelLineas.CANTIDAD, TableModelLineas.PRECIO, TableModelLineas.DESCUENTO, TableModelLineas.NETO, TableModelLineas.IMPORTE};
+//                listLineas.setModel(new TableModelLineas(colsl, model.getListLineasNormales()));
+//                listLineas.setRowHeight(30);
+//                TableColumnModel columnModelLineas = listLineas.getColumnModel();
+//                columnModelLineas.getColumn(0).setPreferredWidth(150);
+//                columnModelLineas.getColumn(1).setPreferredWidth(150);
+//                break;
+//            case Model.FILTER:
+//               Cliente.setText(model.getFilter().getNombre());
+//                break;
         }
         this.panel.revalidate();
     }
